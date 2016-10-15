439d2ce271343f00490637d8c5ac07f75921fb42
Jeff Carvalho <jef.carvalho522@gmail.com>
2016-10-15 02:31:16 -0300

    Characters instantiation and player movement test.

---
+++ Solid Rascal/Solid Rascal/Characters/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Rascal.Characters
+{
+    class Character
+    {
+        public int xPos { get; set; }
+        public int yPos { get; set; }
+        public int aHealth;
+
+    }
+}
